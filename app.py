b96df6f3b6b9aebcfdecd0bfd7ee4fa425c68fa8
AndreeaGardelean <andreea.gardelean.2020@live.rhul.ac.uk>
2021-06-04 09:20:12 +0100

    updates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
 import sqlite3 as sql
+from random import Random
 
 # app - The flask application where all the magical things are configured.
 app = Flask(__name__)
@@ -16,7 +17,7 @@ BUGGY_RACE_SERVER_URL = "http://rhul.buggyrace.net"
 def home():
     return render_template('index.html', server_url=BUGGY_RACE_SERVER_URL)
 #------------------------------------------------------------
-#poster page
+# poster page
 #------------------------------------------------------------
 @app.route('/poster')
 def poster():
@@ -34,7 +35,7 @@ def create_buggy():
         cur = con.cursor()
         cur.execute("SELECT * FROM buggies")
         record = cur.fetchone();
-        return render_template("buggy-form.html", buggy = record)
+        return render_template("buggy-form.html", buggy = None)
 
     elif request.method == 'POST':
         msg=""
@@ -58,6 +59,18 @@ def create_buggy():
         antibiotic = request.form['antibiotic']
         banging = request.form['banging']
         algo = request.form['algo']
+        buggy_id = request.form['id']
+
+# Validation data
+    motive_power = ['Fusion', 'Thermo', 'Solar', 'Wind']
+    type_power = [power_type, aux_power_type]
+    units = [power_units, aux_power_units]
+
+    if power_type in motive_power:
+        power_units = 1
+
+    if aux_power_type in motive_power:
+        aux_power_units = 1
 
 
     name = ['Petrol', 'Fusion', 'Steam', 'Bio', 'Electric', 'Rocket', 'Hamster', 'Thermo', 'Solar', 'Wind']
@@ -76,17 +89,32 @@ def create_buggy():
     capabilities_price = [5, 20, 28, 30]
     total_cost = 0
 
-    #totals = zip(name,value)
+# Calculate the price of the buggy based on the input
+    if aux_power_type != 'Hamster':
+        hamster_booster = 0
+    if power_type != 'Hamster':
+        hamster_booster = 0
+    elif aux_power_type == 'Hamster':
+        aux_power_units = 0
+    elif power_type == 'Hamster':
+        power_units = 0
+
     for c, cp in zip(capabilities, capabilities_price):
         if attack in c:
             total_cost += cp
+
     for e, ec in zip(extra, extra_cost):
         if e == 'True':
             total_cost += ec
 
     for at, ac in zip(armour_type, armour_cost):
+        while int(qty_wheels) != 4:
+            ac += 40*((int(qty_wheels))-4)*1/10
+            print(ac)
+            break
         if armour in at:
             total_cost += ac
+
     for t, p in zip(tyres_type, tyres_price):
         if tyres in t:
             total_cost += int(qty_tyres)*p
@@ -102,22 +130,27 @@ def create_buggy():
     for n in name:
         print('')
 
-        try:
-            with sql.connect(DATABASE_FILE) as con:
-                cur = con.cursor()
+    try:
+        with sql.connect(DATABASE_FILE) as con:
+            cur = con.cursor()
+            if buggy_id:
                 cur.execute(
-                    "UPDATE buggies set qty_wheels=?, power_type=?, power_units=?, aux_power_type=?, aux_power_units=?, hamster_booster=?, flag_color=?, flag_pattern=?, flag_color_secondary=?, tyres=?, qty_tyres=?, armour=?, attack=?, qty_attacks=?, fireproof=?, insulated=?, antibiotic=?, banging=?, total_cost=?, algo=? WHERE id=?",
-                    (qty_wheels, power_type, power_units, aux_power_type, aux_power_units, hamster_booster, flag_color, flag_pattern, flag_color_secondary, tyres, qty_tyres, armour, attack, qty_attacks, fireproof, insulated, antibiotic, banging, algo, total_cost, DEFAULT_BUGGY_ID)
+                    "UPDATE buggies set qty_wheels=?, power_type=?, power_units=?, aux_power_type=?, aux_power_units=?, hamster_booster=?, flag_color=?, flag_pattern=?, flag_color_secondary=?, tyres=?, qty_tyres=?, armour=?, attack=?, qty_attacks=?, fireproof=?, insulated=?, antibiotic=?, banging=?, algo=?, total_cost=? WHERE id=?",
+                    (qty_wheels, power_type, power_units, aux_power_type, aux_power_units, hamster_booster, flag_color, flag_pattern, flag_color_secondary, tyres, qty_tyres, armour, attack, qty_attacks, fireproof, insulated, antibiotic, banging, algo, total_cost, buggy_id)
                 )
-                con.commit()
-                msg = "Record successfully saved"
-                    #msg = f"qty_weels={qty_wheels}  flag_color={flag_color}"
-        except:
-            con.rollback()
-            msg = "error in update operation"
-        finally:
-            con.close()
-        return render_template("updated.html", msg = msg, warning = warning)
+            else:
+                cur.execute(
+                    "INSERT INTO buggies (qty_wheels, power_type, power_units, aux_power_type, aux_power_units, hamster_booster, flag_color, flag_pattern, flag_color_secondary, tyres, qty_tyres, armour, attack, qty_attacks, fireproof, insulated, antibiotic, banging, algo, total_cost) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                    (qty_wheels, power_type, power_units, aux_power_type, aux_power_units, hamster_booster, flag_color, flag_pattern, flag_color_secondary, tyres, qty_tyres, armour, attack, qty_attacks, fireproof, insulated, antibiotic, banging, algo, total_cost,)
+                )
+            con.commit()
+            msg = "Record successfully saved"
+    except:
+        con.rollback()
+        msg = "error in update operation"
+    finally:
+        con.close()
+    return render_template("updated.html", msg = msg, warning = warning)
 
 #------------------------------------------------------------
 # a page for displaying the buggy
@@ -128,16 +161,36 @@ def show_buggies():
     con.row_factory = sql.Row
     cur = con.cursor()
     cur.execute("SELECT * FROM buggies")
-    record = cur.fetchone();
-    return render_template("buggy.html", buggy = record)
+    records = cur.fetchall();
+    if records == None:
+        print('No buggy available')
+    else:
+        return render_template("buggy.html", buggies = records)
 #------------------------------------------------------------
 # a placeholder page for editing the buggy: you'll need
 # to change this when you tackle task 2-EDIT
 #------------------------------------------------------------
-@app.route('/edit')
-def edit_buggy():
-    return render_template("buggy-form.html")
+@app.route('/edit/<buggy_id>')
+def edit_buggy(buggy_id):
+    con = sql.connect(DATABASE_FILE)
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("SELECT * FROM buggies WHERE id=?", (buggy_id,))
+    record = cur.fetchone();
+    return render_template("buggy-form.html", buggy=record)
 
+#------------------------------------------------------------
+# delete route
+#------------------------------------------------------------
+@app.route('/delete/<buggy_id>', methods=['POST', 'GET'])
+def delete_buggy(buggy_id):
+    if request.method == 'POST':
+        con = sql.connect(DATABASE_FILE)
+        con.execute("DELETE FROM buggies WHERE id=?", (buggy_id,))
+        con.commit()
+        return redirect("/buggy")
+    elif request.method == 'GET':
+        return redirect('/')
 #------------------------------------------------------------
 # You probably don't need to edit this... unless you want to ;)
 #
@@ -148,11 +201,22 @@ def edit_buggy():
 #  the data, so in effect jsonify() is rendering the data.
 #------------------------------------------------------------
 @app.route('/json')
-def summary():
+def get_json():
     con = sql.connect(DATABASE_FILE)
     con.row_factory = sql.Row
     cur = con.cursor()
-    cur.execute("SELECT * FROM buggies WHERE id=? LIMIT 1", (DEFAULT_BUGGY_ID))
+    cur.execute("SELECT * FROM buggies")
+    records = cur.fetchall();
+    return render_template("json.html", buggies = records)
+#------------------------------------------------------------
+#
+#------------------------------------------------------------
+@app.route('/json/<buggy_id>')
+def summary(buggy_id):
+    con = sql.connect(DATABASE_FILE)
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("SELECT * FROM buggies WHERE id=? LIMIT 1", (buggy_id,))
 
     buggies = dict(zip([column[0] for column in cur.description], cur.fetchone())).items()
     return jsonify({ key: val for key, val in buggies if (val != "" and val is not None) })
